02038e278a8c958aef2ed71bdab575f935b00f27
liamfenn <lgf63569@uga.edu>
2024-09-23 10:52:17 -0400

    Created new file structure, added 'free-look, free-roam' functionality

--- scripts/scenes/mainScene.js
+++ scripts/scenes/mainScene.js
@@ -1,7 +1,4 @@
-const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas, true);
-
-const createScene = function () {
+const createScene = function (engine, canvas) {
     const scene = new BABYLON.Scene(engine);
 
     // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
@@ -13,23 +10,26 @@ const createScene = function () {
     // Attach the camera to the canvas.
     camera.attachControl(canvas, true);
 
+    // Enable camera controls
+    camera.keysUp.push(87);    // W
+    camera.keysDown.push(83);  // S
+    camera.keysLeft.push(65);  // A
+    camera.keysRight.push(68); // D
+
     // Create a basic light, aiming 0,1,0 - meaning, to the sky.
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Create a built-in "box" shape; its constructor takes 6 params: name, width, height, depth, subdivisions, scene.
     const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 
+    // Move the box upward 1/2 of its height.
     box.position.y = 1;
+
+    // Add interactivity
+    box.actionManager = new BABYLON.ActionManager(scene);
+    box.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) {
+        alert("Box clicked!");
+    }));
 
     return scene;
 };
-
-const scene = createScene();
-
-engine.runRenderLoop(function () {
-    scene.render();
-});
-
-window.addEventListener("resize", function () {
-    engine.resize();
-});
